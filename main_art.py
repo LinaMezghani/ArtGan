13c5a1e803bc0c6ab7f3d57ca8aefdef4b3fd069
Lina Mezghani <35572460+LinaMezghani@users.noreply.github.com>
2019-01-09 20:46:38 +0100

    Update main_art.py

--- main_art.py
+++ main_art.py
@@ -20,8 +20,8 @@ from tqdm import tqdm
 from networks import get_models, adversarial, null_loss, generator, discriminator
 from utils import producer, getPaths, scale, mean
 
-data_train = r"C:\Users\tgill\OneDrive\Documents\GD_AI\ArtGAN\wikipaintings_full\wikipaintings_train"
-data_test = r"C:\Users\tgill\OneDrive\Documents\GD_AI\ArtGAN\wikipaintings_full\wikipaintings_train"
+data_train = "data/wikipaintings_train"
+data_test = "data/wikipaintings_test"
 
 train_paths, y_train, classes = getPaths(data_train)
 test_paths, y_test, classes = getPaths(data_test)
